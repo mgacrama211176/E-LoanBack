1b24da6fb818e5d2b3d53d2611d4bf25e8d23779
Marlon Gacrama Jr <mgacrama_ccs@uspf.edu.ph>
2023-05-14 17:29:22 +0800

    Updated Hadler Put fuction

--- routes/handlersRoute.js
+++ routes/handlersRoute.js
@@ -1,5 +1,4 @@
 import express from "express";
-import InvestorModel from "../models/InvestorModel.js";
 
 import desirealize from "../middleware/desirealize.js";
 import HttpSuccessCode from "../utils/HttpSuccessCodes.js";
@@ -21,7 +20,7 @@ router.post("/", async (request, response) => {
     const updateAdmin = await adminModel.findByIdAndUpdate(
       request.body.managerId,
       {
-        $push: { handlers: request.params.id },
+        $push: { handlers: data._id },
       },
       { new: true }
     );
@@ -42,26 +41,25 @@ router.get("/", async (request, response) => {
 });
 
 //Deleting Handler
-router.delete("/:id", async (request, response) => {
-  router.delete("/:id", async (request, response) => {
-    try {
-      const removeInvestor = await HandlerModel.findByIdAndDelete(
-        request.params.id
-      );
 
-      //Update Admin Data for Investor
-      const updateAdmin = await adminModel.findByIdAndUpdate(
-        request.body.managerId,
-        {
-          $pull: { handlers: request.params.id },
-        },
-        { new: true }
-      );
-      response.status(200).json({ removeInvestor, updateAdmin });
-    } catch (err) {
-      response.status(500).json(err);
-    }
-  });
+router.delete("/:id", async (request, response) => {
+  try {
+    const removeInvestor = await HandlerModel.findByIdAndDelete(
+      request.params.id
+    );
+
+    //Update Admin Data for Investor
+    const updateAdmin = await adminModel.findByIdAndUpdate(
+      request.body.managerId,
+      {
+        $pull: { handlers: request.params.id },
+      },
+      { new: true }
+    );
+    response.status(200).json({ removeInvestor, updateAdmin });
+  } catch (err) {
+    response.status(500).json(err);
+  }
 });
 
 //Update Investor Information
